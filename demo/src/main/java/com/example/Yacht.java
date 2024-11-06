3853692d58fde081b6895b2aa770a245f9f62d82
kim001hs <kim001hs@gmail.com>
2024-11-06 15:48:16 +0900

    new file:   .vscode/settings.json 	new file:   demo/pom.xml 	new file:   demo/src/main/java/com/example/PlayerCountServlet.java 	new file:   demo/src/main/java/com/example/Yacht.java 	new file:   demo/src/main/webapp/WEB-INF/JSPtest.jsp 	new file:   demo/src/main/webapp/WEB-INF/web.xml 	new file:   demo/src/main/webapp/css/style.css 	new file:   demo/src/main/webapp/default.HTML 	new file:   demo/src/main/webapp/js/JStest.js 	new file:   demo/src/test/java/com/example/AppTest.java 	new file:   demo/target/classes/com/example/PlayerCountServlet.class 	new file:   demo/target/classes/com/example/Yacht.class 	new file:   demo/target/demo-1.0-SNAPSHOT.jar 	new file:   demo/target/maven-archiver/pom.properties 	new file:   demo/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   demo/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	new file:   demo/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/createdFiles.lst 	new file:   demo/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst 	new file:   demo/target/surefire-reports/TEST-com.example.AppTest.xml 	new file:   demo/target/surefire-reports/com.example.AppTest.txt 	new file:   demo/target/test-classes/com/example/AppTest.class

---
+++ demo/src/main/java/com/example/Yacht.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Yacht 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println( "Hello World!" );
+    }
+}
